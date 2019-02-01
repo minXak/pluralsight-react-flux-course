e9bdb48e80da6315abddb696e4be6eb89758fd0a
Michail Knyrevic <michail@webilize.com>
2019-02-01 18:32:38 +0100

    Added form

---
+++ src/components/authors/manageAuthorPage.js
@@ -0,0 +1,92 @@
+"use strict";
+
+var React = require('react');
+var Router = require("react-router");
+var Toastr = require("toastr");
+
+var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
+
+var ManageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
+    statics: {
+        willTransitionFrom: function (transition, component) {
+            if (component.state.dirty) {
+                if (!confirm('Leave without saving?')) {
+                    transition.abort();
+                }
+            }
+        }
+    },
+    componentWillMount: function () {
+        var authorId = this.props.params.id;
+        if (authorId) {
+            this.setState({ author: AuthorApi.getAuthorById(authorId) });
+        } else {
+            // this.transitionTo("notFound");
+        }
+    },
+
+    getInitialState: function () {
+        return {
+            author: { id: '', firstName: '', lastName: '' },
+            errors: {},
+            dirty: false
+        };
+    },
+    setAuthorState: function (event) {
+        this.setState({ dirty: true });
+        var field = event.target.name;
+        var value = event.target.value;
+        this.state.author[field] = value;
+        return this.setState({ author: this.state.author });
+    },
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {};
+
+        if (this.state.author.firstName.length < 3) {
+            this.state.errors.firstName = "First name length";
+            formIsValid = false;
+        }
+
+        if (this.state.author.lastName.length < 3) {
+            this.state.errors.lastName = "Last name length";
+            formIsValid = false;
+        }
+
+        this.setState({ errors: this.state.errors });
+        return formIsValid;
+    },
+
+    saveAuthor: function (event) {
+        event.preventDefault();
+
+        if (!this.authorFormIsValid()) {
+            return;
+        }
+
+        AuthorApi.saveAuthor(this.state.author);
+        this.setState({ dirty: false });
+
+        Toastr.success("Author saved.");
+        this.transitionTo('authors');
+
+    },
+    render: function () {
+        return (
+            <div>
+                <h1>Manage Author</h1>
+                <AuthorForm
+                    author={this.state.author}
+                    onChange={this.setAuthorState}
+                    onSave={this.saveAuthor}
+                    errors={this.state.errors} />
+            </div>
+        );
+    }
+});
+
+module.exports = ManageAuthorPage;
--- src/routes.js
+++ src/routes.js
@@ -10,15 +10,22 @@ var Route = Router.Route;
 
 var App = require('./components/app');
 var Home = require('./components/homePage');
+
 var About = require('./components/about/aboutPage');
 var Authors = require('./components/authors/authorPage');
+var ManageAuthorPage = require('./components/authors/manageAuthorPage');
+
 var NotFoundPage = require('./components/404');
 
 var routes = (
     <Route name="app" path="/" handler={App}>
         <DefaultRoute handler={Home}></DefaultRoute>
+        
         <Route name="about" handler={About}></Route>
         <Route name="authors" handler={Authors}></Route>
+        <Route name="addAuthor" path="author" handler={ManageAuthorPage}></Route>
+        <Route name="manageAuthor" path="author/:id" handler={ManageAuthorPage}></Route>
+
         <NotFoundRoute handler={NotFoundPage}></NotFoundRoute>
 
         <Rediret from="about-us" to="about"></Rediret>
